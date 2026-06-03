82a3360266a92a3681e9004e27c9fac620fcbea4
Mathieu Fortin <mathieu.fortin.re@gmail.com>
2026-06-03 13:56:48 -0400

    Updating file header.

--- src/lerfob/carbonbalancetool/CarbonAccountingTool.java
+++ src/lerfob/carbonbalancetool/CarbonAccountingTool.java
@@ -1,12 +1,14 @@
 /*
  * This file is part of the lerfob-foresttools library.
  *
- * Copyright (C) 2010-2014 Mathieu Fortin for LERFOB AgroParisTech/INRA, 
- *
+ * Copyright (C) 2010-2019 Mathieu Fortin for LERFOB AgroParisTech/INRA, 
+ * Copyright (C) 2019-2026 His Majesty the King in right of Canada
+ * Author: Mathieu Fortin, Canadian Forest Service
+ * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * version 3 of the License, or (at your option) any later version.
  *
  * This library is distributed with the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied
